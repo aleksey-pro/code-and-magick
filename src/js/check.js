8608a4d1591159f21a2fea517e67025b9db297bb
dave_gahan <anxieter@yandex.ru>
2016-10-12 20:02:54 +0300

    написан код вывода сообщений

--- src/js/check.js
+++ src/js/check.js
@@ -1,29 +1,33 @@
 'use strict'
 
 function getMessage(a, b){
-	var typeOfA = typeof a;
-	if (typeof a == "boolean") {
-		if (typeOfA === true) {
-			return "Я попал в " + b;
-		}
-		else {
+  if (typeof a == 'boolean') {
+    if (a) {
+      return "Я попал в " + b;
+    } else {
       return "Я никуда не попал";
     }
-	}if (typeOfA == 'number'){
-		  return "Я прыгнул на " + a * 100 + " сантиметров";
-	}if (Array.isArray(a) && !Array.isArray(b)){
-				var numberOfSteps = 0;
-				for (var i =0; i < a.length; i++) {
-					 numberOfSteps += a[i];
-				}
-          return "Я прошел " + numberOfSteps + " шагов";
-	}if (Array.isArray(a) && Array.isArray(b)){
+  }
+
+  if (typeof a == 'number') {
+    return "Я прыгнул на " + a * 100 + " сантиметров";
+  }
+
+  if (Array.isArray(a) && !Array.isArray(b)) {
+    var numberOfSteps = 0;
+    for (var i = 0; i < a.length; i++) {
+      numberOfSteps += a[i];
+    }
+      return "Я прошел " + numberOfSteps + " шагов";
+  }
+
+  if (Array.isArray(a) && Array.isArray(b)) {
     var distancePath = 0;
-    for (i =0; i < a.length; i++){
-    distancePath += a[i] * b[i];
-  }
+    for (var i = 0; i < a.length; i++){
+      distancePath += a[i] * b[i];
+    }
     return "Я прошел " + distancePath + " метров";
-	}else{
-    return "Переданы некорректные данные";
   }
+
+  return "Переданы некорректные данные";
 }
