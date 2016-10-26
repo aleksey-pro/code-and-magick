c71008e35fda56fac2880c8ed1a5851d15e325d1
dave_ghan <anxieter@yandex.ru>
2016-10-27 01:51:42 +0400

    Переделан код валидации

--- src/js/form.js
+++ src/js/form.js
@@ -1,44 +1,64 @@
 'use strict';
 
 window.form = (function() {
-  var formContainer = document.querySelector('.overlay-container');
-  var formCloseButton = document.querySelector('.review-form-close');
-  var formRequired = document.querySelector('.review-form');
-  var inputsGroup = document.querySelector('.review-form-group');
+  var formContainer = document.querySelector('.overlay-container'), // див в котором форма
+    formCloseButton = document.querySelector('.review-form-close'), // кнопка закрытия
+    formRequired = document.querySelector('.review-form'), // форма
+    inputsGroup = document.querySelector('.review-form-group'), // группа заполняемых полей
+    SubmitButton = document.querySelector('.review-submit'), // кнопка
+    requiredText = document.getElementById('review-text'),  // требуемое поле Отзыв
+    requiredName = document.getElementById('review-name'), // требуемое поле Имя
+    hidingName = document.querySelector('.review-fields-name'), // скрываемое имя
+    hidingTstml = document.querySelector('.review-fields-text');//скрываемый отзыв
 
-  function validate() {
-    var elem = document.getElementById('review-text');
-    elem.required = Number(formRequired['review-mark'].value) < 3;
-  }
+  //  Тут есть недотчет. Если человек ставит высокую оценку, пишет отзыв,
+  // а потом ставит низкую оценку - форма снова требует отзыв. Но это мне кажется логично,
+  // так как раз человек снизил оценку, значит у него мнение поменялось, поэтому и отзыв пиши другой.
+
+  // Изменилось количество звездочек
+
+  SubmitButton.setAttribute('disabled', 'disabled'); // кнопка задисейблена
+
   inputsGroup.onchange = function() {
-    validate();
-  };
-  validate();
-
-  var requiredName = document.getElementById('review-name');
-  requiredName.required = true;
-  requiredName.oninput = function() {
-    document.querySelector('.review-fields-name').classList.add('invisible');
-  };
-  var requiredText = document.getElementById('review-text');
-  requiredText.oninput = function() {
-    document.querySelector('.review-fields-text').classList.add('invisible');
-  };
-
-  function validateSubmit(event) {
-    if (requiredName.validity.valueMissing && requiredText.validity.valueMissing) {
-      event.preventDefault();
-      document.querySelector('.review-submit').disabled = true;
+    // Добавить/убрать required к полю отзыв
+    requiredText.required = Number(formRequired['review-mark'].value) < 3;
+// Обновить блок .review-fields. Показать/скрыть лэйбл "отзыв" (скрыл в случае если поле "Отзыв" не требуется)
+    if (!requiredText.required ) {
+      hidingTstml.classList.add('invisible');
     } else {
-      document.querySelector('.review-fields').classList.add('invisible');
+      hidingTstml.classList.remove('invisible');
+    }
+  };//onchange
+
+  function validate(label, requiredField) {
+// Проверить значения поля имя и отзыв, если оба валидны то снять дизейбл с кнопки отправки
+// (добавил проверку имени, в отзыве могут быть любые знаки по сути)
+    if (requiredText.value !== '' || requiredName.value !== '' && (/^[А-Яа-я0-9]+$/.test(requiredName.value))) {
+      SubmitButton.removeAttribute('disabled');
+    } else {
+// Иначе добавить дизейбл к кнопке отправки
+      SubmitButton.setAttribute('disabled', 'disabled');
+    }
+// Обновить блок .review-fields. Показать/скрыть лэйбл "отзыв"
+    if (requiredField.value !== '') {
+      label.classList.add('invisible');
+    } else {
+      label.classList.remove('invisible');
     }
   }
-  requiredText.addEventListener('keyup', validateSubmit);
-  requiredName.addEventListener('keyup', validateSubmit);
+// Изменилось значение поля "имя"
+  requiredName.oninput = function() {
+    validate(hidingName, requiredName);
+  };
+
+// // Изменилось значение поля "отзыв"
+
+  requiredText.oninput = function() {
+    validate(hidingTstml, requiredText);
+  };
 
   var form = {
     onClose: null,
-
     /**
      * @param {Function} cb
      */
@@ -46,10 +66,8 @@ window.form = (function() {
       formContainer.classList.remove('invisible');
       cb();
     },
-
     close: function() {
       formContainer.classList.add('invisible');
-
       if (typeof this.onClose === 'function') {
         this.onClose();
       }
