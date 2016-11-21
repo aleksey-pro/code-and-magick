fe82f1c5c63754c007ae1b783c4e2170dded1e37
dave_ghan <anxieter@yandex.ru>
2016-11-21 16:32:27 +0300

    Написан код конструктора Review, ошибка с review

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -2,12 +2,27 @@
 
 //отрисовка всего списка
 
-define(['./review'], function(getReviewsElement) {
-  var container = document.querySelector('.reviews-list');
-  var renderReviews = function(data) {
-    data.forEach(function(review) {
-      container.appendChild(getReviewsElement(review));
-    });
+define(['./load', './review'], function(load, getReviewsElement) {
+  var Review = function(data) {
+    data = this.data;
+    this.element = getReviewsElement(data);
   };
-  return renderReviews;
+  Review.prototype.setActiveLink = function() {
+    this.quizContainer = document.querySelector('.review-quiz');
+    this.quizElems = document.querySelectorAll('.review-quiz-answer');
+    var self = this;
+    self.quizContainer.onclick = function(event) {
+      if (event.target.tagName !== 'SPAN') return;
+      for (var i = 0; i < self.quizElems.length; i++) {
+        if (self.quizElems[i].classList.contains('active')) {
+          self.quizElems[i].classList.remove('active');
+        }
+        event.target.classList.add('active');
+      }
+    };
+  };
+  Review.prototype.removeActiveLink = function() {
+    self.quizContainer.onclick = null;
+  };
+  return Review;
 });
