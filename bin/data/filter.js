a7d17de35e95845c43ae7e614f9656a4550385e5
dave_ghan <anxieter@yandex.ru>
2017-01-17 02:56:58 +0300

    Создана фильтрация отзывов

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -1,5 +1,31 @@
 'use strict';
 
 module.exports = function(list, filterID) {
-  return list;
+  switch(filterID) {
+    case 'reviews-all':
+      return list;
+    case 'reviews-recent':
+      return list.sort(function(a, b) {
+        return a.created - b.created;
+      });
+    case 'reviews-good':
+      var goodData = list.filter(function(number) {
+        return number.rating > 2;
+      });
+      return goodData.sort(function(a, b){
+        return b.rating - a.rating;
+      });
+    case 'reviews-bad':
+      var badData = list.filter(function(number) {
+        return number.rating < 3;
+      });
+      return badData.sort(function(a, b) {
+        return a.rating - b.rating;
+      });
+    case 'reviews-popular':
+      return list.sort(function(a, b) {
+        return b.review_usefulness - a.review_usefulness;
+      });
+  }
 };
+
--- src/js/load.js
+++ src/js/load.js
@@ -2,23 +2,24 @@
 
 //загрузка с сервера
 
-define(['./reviews'], function() {
-  return function(url, callback, JSONPCallback) {
-    if (!JSONPCallback) {
-      JSONPCallback = 'cb' + Date.now();
-    }
-    window[JSONPCallback] = function(data) {
-      callback(data);
-      script.parentNode.removeChild(script);
-      delete window[JSONPCallback];
-    };
-    var script = document.createElement('script');
-    script.src = url + '?callback=' + JSONPCallback;
-    document.body.appendChild(script);
+define(function() {
+  var getSearchString = function(params) {
+    return Object.keys(params).map(function(param) {
+      return [param, params[param]].join('=');
+    }).join('&');
+  };
 
-    script.onerror = function() {
-      this.parentNode.removeChild(script);
-      delete window[JSONPCallback];
+  return function(url, params, callback) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onload = function(evt) {
+      var loadedData = JSON.parse(evt.target.response);
+      callback(loadedData);
     };
+
+    xhr.open('GET', url + '?' + getSearchString(params));
+
+    xhr.send();
   };
 });
+
